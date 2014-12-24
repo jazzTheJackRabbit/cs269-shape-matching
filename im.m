30351ac93faaae8a01f3ba69fa506f7116930536
The Jazz <amogh.91@gmail.com>
2014-12-24 00:48:43 -0800

    Removed old files

--- im.m
+++ im.m
@@ -1,13 +1,4 @@
 function im(X,vec);
-% im(X) or im(X,[minval maxval])
-%
-% Handy function for showing a grayscale image with a colorbar and
-% interactive pixel value tool
-%
-% Serge Belongie
-% 20-Aug-2000
-% sjb@eecs.berkeley.edu
- 
 if nargin==1
    imagesc(X)
 else
